81e3ee226bfa558bdfec78f3c2f2537752ce4a97
kingkouda <120647091+mysterus44@users.noreply.github.com>
2025-06-04 15:08:31 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,25 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("abjad-cache").then((cache) =>
-      cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json",
-        "./logo.jpg",
-        "./service-worker.js",
-      ])
-    )
+const CACHE_NAME = "abajad-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/whats.png",
+  "/manifest.json",
+  "/logo.png",
+  // Ajoute ici tout autre fichier JS, CSS, image, etc.
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
